3801af635136df9bb5518723671b1049fa49aae0
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2019-03-25 20:44:01 +0100

    ejemplo5

---
+++ ejemplo5/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template,abort
+app = Flask(__name__)	
+
+@app.route('/',methods=["GET","POST"])
+def inicio():
+	lista = ["manzana","platano","piña","kiwi"]
+	return render_template("inicio.html", lista=lista)
+
+
+
+app.run(debug=True)
