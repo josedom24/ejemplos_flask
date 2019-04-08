0af076d88b341cea3781e19f172effacd7705fe3
José Domingo <josedom24@gmail.com>
2019-04-08 13:45:12 +0200

    ejemplo14

---
+++ ejemplo14/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,request
+app = Flask(__name__)	
+
+
+@app.route('/',methods=["GET","POST"])
+def inicio():
+	return render_template("formulario.html")
+
+@app.route("/procesar", methods=["post"])
+def procesar_formulario():
+	passwd = request.form.get("pass_control")
+	if passwd == "asdasd":
+		datos = request.form
+		return render_template("datos.html", datos=datos)
+	else:
+		return render_template("error.html", error="Contraseña incorrecta")
+
+app.run(debug=True)
