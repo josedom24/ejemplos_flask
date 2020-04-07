1218301b3ea814ecb9b7a30ff5fb9f4ddba9556e
José Domingo Muñoz <josedom24@gmail.com>
2020-04-07 11:04:14 +0200

    ejemplo12

--- ejemplo12/app.py
+++ ejemplo12/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request
 app = Flask(__name__)	
 
 
-@app.route('/',methods=["GET","POST"])
+@app.route('/',methods=["GET"])
 def inicio():
 	return render_template("inicio.html")
 
